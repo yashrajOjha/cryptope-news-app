37d43003e885b6b703b92a17dca96d10980664d0
Yash <yashrajojah28@gmail.com>
2022-01-29 22:46:30 +0530

    UPDATE: Responsive Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState,useEffect } from 'react';
 import {Button, Menu, Typography, Avatar} from 'antd';
 import {Link} from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
@@ -7,17 +8,39 @@ import icon from '../images/logocrypto.png';
 import MenuItem from 'antd/lib/menu/MenuItem';
 
 const Navbar = () => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(undefined);
+  
+    useEffect(() => {
+      const handleResize = () => setScreenSize(window.innerWidth);
+  
+      window.addEventListener('resize', handleResize);
+  
+      handleResize();
+  
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    useEffect(() => {
+      if (screenSize <= 800) {
+        setActiveMenu(false);
+      } else {
+        setActiveMenu(true);
+      }
+    }, [screenSize]);
+
   return( <div className='nav-container'>
       <div className='logo-container'>
           <Avatar src={icon} size="large"/>
           <Typography.Title level={2} className='logo'>
               <Link to="/">Crypto Pe</Link>
           </Typography.Title>
-          {/* <Button className='menu-control-container'>
-
-          </Button> */}
+          <Button className='menu-control-container' onClick={()=> setActiveMenu(!activeMenu)}>
+              <MenuOutlined/>
+          </Button>
       </div>
-      <Menu theme="dark">
+      {activeMenu && (
+          <Menu theme="dark">
           <Menu.Item icon={<HomeOutlined/>}>
               <Link to='/'>Home</Link>
           </Menu.Item> 
@@ -31,6 +54,7 @@ const Navbar = () => {
               <Link to='/news'>News</Link>
           </Menu.Item> 
       </Menu>
+      )}
   </div>);
 };
 
