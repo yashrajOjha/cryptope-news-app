4a9578eb2bd83c1b1baab116035c330e5de33e5b
Yash <yashrajojah28@gmail.com>
2022-01-29 23:05:58 +0530

    Simple index.html changes

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import HTMLReactParser from 'html-react-parser';
+
+import { useGetExchangesQuery } from '../services/cryptoApi';
+import Loader from './Loader';
+import Title from 'antd/lib/skeleton/Title';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
-  return <div>Exchanges</div>;
+  const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
+ // Note: To access this endpoint you need premium plan
+  if (isFetching) return <Loader />;
+
+  return (
+    <>
+        <Typography.Title level={4}>
+        Buy the premium plan of Coinranking API to access exchanges.
+        </Typography.Title>
+    </>
+  );
 };
 
 export default Exchanges;
